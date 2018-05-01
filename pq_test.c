0d90eeccb09112b63832629ce8cbccc0f680738e
nchronas <nchronas@gmail.com>
2018-05-01 11:57:36 +0200

    main c renamed

--- pq_test.c
+++ pq_test.c
@@ -57,32 +57,10 @@ void *mainThread(void *arg0)
     GPIO_init();
     UART_init();
 
-    /* Configure the LED pin */
-    GPIO_setConfig(Board_GPIO_LED0, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
 
-    /* Turn on user LED */
-    GPIO_write(Board_GPIO_LED0, Board_GPIO_LED_ON);
-
-    /* Create a UART with data processing off. */
-    UART_Params_init(&uartParams);
-    uartParams.writeDataMode = UART_DATA_BINARY;
-    uartParams.readDataMode = UART_DATA_BINARY;
-    uartParams.readReturnMode = UART_RETURN_FULL;
-    uartParams.readEcho = UART_ECHO_OFF;
-    uartParams.baudRate = 115200;
-
-    uart = UART_open(Board_UART0, &uartParams);
-
-    if (uart == NULL) {
-        /* UART_open() failed */
-        while (1);
-    }
-
-    UART_write(uart, echoPrompt, sizeof(echoPrompt));
 
     /* Loop forever echoing */
     while (1) {
-        UART_read(uart, &input, 1);
-        UART_write(uart, &input, 1);
+
     }
 }
